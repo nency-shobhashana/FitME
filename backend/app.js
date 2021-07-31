c69a1d0b4ce2ff287fbf0445e2c9fd4342d18d75
Nency Shobhashana <shobhashananency@gmail.com>
2021-07-31 19:36:54 -0400

    atabase connection in backend

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+require('dotenv/config');
+app.use(bodyParser.json());
+
+app.use(cors());
+app.options('*', cors());
+
+mongoose.set('useFindAndModify', false);
+
+//Import Routes
+
+// const categoryRoute = require('./routes/category');
+
+//Routes
+
+app.get('/', (req, res) => {
+    res.send("Welcome to Home");
+})
+
+
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, (err) =>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log('Connected to db');
+    }
+    // app.use('/category', categoryRoute);
+})
+
+app.listen(3000);
