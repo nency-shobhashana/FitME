948dfca4f7742f79cf9c4b576917adc21f31ae68
aparna124 <41099165+aparna124@users.noreply.github.com>
2021-08-04 17:27:09 -0400

    Updated Home page of user

--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react'
-import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View, Image, TextInput } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View, Image, TextInput, Dimensions } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import {firebaseApp} from '../firebase-config';
 import axios from "axios";
 import { HOST_URL } from '../commonConfig'
 import { StackActions, NavigationActions } from 'react-navigation'; 
-import {Card} from 'react-native-shadow-cards';
-
 
 class Home extends React.Component {
 
@@ -73,102 +71,101 @@ class Home extends React.Component {
     <ScrollView>
       <View style={styles.container}>
       
-      {/* <Card style={{width: '100%', height: 40, paddingTop: 10, marginTop: 10, alignItems: 'center', justifyContent: 'center'}}>
-        <Text style={styles.dateText}>{new Date().getDate()}/{new Date().getMonth() + 1}/{new Date().getFullYear()}</Text>
-      </Card>
+      <View style ={styles.topCardStyle}>
+          <Text style={styles.dateText}>{new Date().getDate()}/{new Date().getMonth() + 1}/{new Date().getFullYear()}</Text>
+      </View>
 
-      <Card style={{width: '100%', height: 180, paddingTop: 10, marginTop: 10}}>
-        <View style={{alignItems: 'center'}}>
-            <Text style={styles.bmiText}>Your Current BMI is {this.state.currentBmi}</Text>
-        </View>
-        
-        <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', padding: 20 }}>
-
-             <Image source={require('../assets/clock.png')}/>
-
-            <View style={{alignItems: 'center', padding: 10}}>
-                <Text style={{ fontWeight: 'bold', marginBottom: 10, fontSize: 15}}>“The Groundwork 
-                for all Happiness” </Text> 
+      <View style ={styles.bmiCardStyle}>
+          <View style={{alignItems: 'center'}}>
+                <Text style={styles.bmiText}>Your Current BMI is {this.state.currentBmi}</Text>
             </View>
-        </View>
-        <View style={{justifyContent: 'center', alignItems: 'center'}}>
-            <TouchableOpacity style={styles.roundButton1}>
-                <Text style={styles.roundBtnText}>+</Text>
-            </TouchableOpacity>
-        </View>
-      </Card>
+            
+            <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', padding: 20 }}>
 
+                <Image source={require('../assets/clock.png')}/>
+
+                <View style={{alignItems: 'center', padding: 10}}>
+                    <Text style={{ fontWeight: 'bold', marginBottom: 10, fontSize: 15}}>“The Groundwork 
+                    for all Happiness” </Text> 
+                </View>
+            </View>
+            <View style={{justifyContent: 'center', alignItems: 'center'}}>
+                <TouchableOpacity style={styles.roundButton1}>
+                    <Text style={styles.roundBtnText}>+</Text>
+                </TouchableOpacity>
+            </View>
+      </View>
 
       <Text style={[styles.text_section, { margin: 10 }]}>Recipes</Text>
 
       <View style={{ display: 'flex', flexDirection: 'row', justifyContent: "space-between"}}>
-            <Card style={[styles.cardStyle]}>
 
-                    <View style={{alignItems: 'center', padding: 10}}>
-                        <Image source={require('../assets/recipe1.png')}/>
-                    </View>
+          <View style ={styles.recipeCardStyle}>
+              <View style={{alignItems: 'center', padding: 10}}>
+                  <Image source={require('../assets/recipe1.png')}/>
+              </View>
 
-                    <View style={{alignItems: 'center', padding: 10}}>
-                        <Text style={{marginBottom: 10, fontSize: 20, color: '#000'}}>Tacos Dishes</Text> 
-                    </View>
-            </Card>
-
-            <Card style={[styles.cardStyle]}>
-                    <View style={{alignItems: 'center', padding: 10}}>
-                        <Image source={require('../assets/recipe2.png')}/>
-                    </View>
-
-                    <View style={{alignItems: 'center', padding: 10}}>
-                        <Text style={{marginBottom: 10, fontSize: 20, color: '#000'}}>Salads</Text> 
-                    </View>
-            </Card>
-    </View>
-
-    <View style={{ display: 'flex', flexDirection: 'row', justifyContent: "space-between"}}>
-            <Card style={[styles.cardStyle]}>   
-
-                    <View style={{alignItems: 'center', padding: 10}}>
-                        <Image source={require('../assets/recipe3.png')}/>
-                    </View>
-
-                    <View style={{alignItems: 'center', padding: 10}}>
-                        <Text style={{marginBottom: 10, fontSize: 20, color: '#000'}}>Keto Dishes</Text> 
-                    </View>
-            </Card>
-
-            <Card style={[styles.cardStyle]}>
-                    <View style={{alignItems: 'center', padding: 10}}>
-                        <Image source={require('../assets/recipe4.png')}/>
-                    </View>
-
-                    <View style={{alignItems: 'center', padding: 10}}>
-                        <Text style={{marginBottom: 10, fontSize: 20, color: '#000'}}>Rice Bowls</Text> 
-                    </View>
-            </Card>
-    </View>
+              <View style={{alignItems: 'center', padding: 10}}>
+                  <Text style={{marginBottom: 10, fontSize: 20, color: '#000'}}>Tacos Dishes</Text> 
+              </View>
+          </View>
 
 
-      <Card style={{width: '100%', height: 100, paddingTop: 10, marginTop: 10, backgroundColor: '#ED7A50'}}>
+          <View style ={styles.recipeCardStyle}>
+              <View style={{alignItems: 'center', padding: 10}}>
+                    <Image source={require('../assets/recipe2.png')}/>
+              </View>
+
+              <View style={{alignItems: 'center', padding: 10}}>
+                    <Text style={{marginBottom: 10, fontSize: 20, color: '#000'}}>Salads</Text> 
+              </View>
+          </View>
+
+      </View>
+
+
+      <View style={{ display: 'flex', flexDirection: 'row', justifyContent: "space-between"}}>
+
+          <View style ={styles.recipeCardStyle}>
+              <View style={{alignItems: 'center', padding: 10}}>
+                  <Image source={require('../assets/recipe1.png')}/>
+              </View>
+
+              <View style={{alignItems: 'center', padding: 10}}>
+                  <Text style={{marginBottom: 10, fontSize: 20, color: '#000'}}>Tacos Dishes</Text> 
+              </View>
+          </View>
+
+
+          <View style ={styles.recipeCardStyle}>
+              <View style={{alignItems: 'center', padding: 10}}>
+                    <Image source={require('../assets/recipe2.png')}/>
+              </View>
+
+              <View style={{alignItems: 'center', padding: 10}}>
+                    <Text style={{marginBottom: 10, fontSize: 20, color: '#000'}}>Salads</Text> 
+              </View>
+          </View>
+      </View>
+
+       
+      <View style ={styles.bottomCardStyle}>
         <View style={{ display: 'flex', flexDirection: 'row', padding: 20 }}>
+          <View style={{alignItems: 'center', padding: 10}}>
+              <Image source={require('../assets/book.png')}/>
+          </View>
 
-            <View style={{alignItems: 'center', padding: 10}}>
-                <Image source={require('../assets/book.png')}/>
-            </View>
-
-            <View style={{alignItems: 'center', padding: 10}}>
-                <Text style={{marginBottom: 10, fontSize: 20, color: '#fff'}}>Learn more about it!</Text> 
-            </View>
-
+          <View style={{alignItems: 'center', padding: 10}}>
+              <Text style={{marginBottom: 10, fontSize: 20, color: '#fff'}}>Learn more about it!</Text> 
+          </View>
         </View>
-      </Card> */}
-
+      </View>
       
-
-        <View style={styles.profileFooterBtn}>
+      <View style={styles.profileFooterBtn}>
           <TouchableOpacity style={styles.button} onPress={() => this.signOutUser()}>
             <Text>Logout</Text>
           </TouchableOpacity>
-        </View>
+      </View>
 
       </View>
     </ScrollView>
@@ -178,12 +175,13 @@ class Home extends React.Component {
 }
 
 
+const deviceWidth = Math.round(Dimensions.get('window').width);
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 10,
-    //paddingTop: 50,
-    //backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   button: {
     margin: 12,
@@ -194,6 +192,7 @@ const styles = StyleSheet.create({
     borderColor: '#EB6C3E',
     borderWidth: 1,
     backgroundColor: '#EB6C3E',
+    width: '100%',
   },
 
   dateText:
@@ -214,6 +213,7 @@ const styles = StyleSheet.create({
   {
     fontSize: 16,  
     color: '#EB6C3E',
+    margin: 8,
   },
 
   profileFooterBtn:
@@ -224,11 +224,78 @@ const styles = StyleSheet.create({
     fontSize: 20,
   },
 
-  cardStyle:
+  topCardStyle:
   {
-    width: '49%', 
-    height: 180, 
-    marginTop: 10
+    width: deviceWidth-25,
+    backgroundColor: "#fff",
+    height: 40,
+    borderRadius: 6,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 5,
+      height: 5
+    },
+    shadowOpacity: 0.25,
+    elevation: 9,
+    shadowRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  bmiCardStyle:
+  {
+    marginTop: 8,
+    width: deviceWidth-25,
+    backgroundColor: "#fff",
+    height: 200,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 5,
+      height: 5
+    },
+    shadowOpacity: 0.25,
+    elevation: 9,
+    shadowRadius: 5
+  },
+
+  recipeCardStyle:
+  {
+    margin: 5,
+    marginTop: 8,
+    width: ((deviceWidth-25)/2)-10,
+    backgroundColor: "#fff",
+    height: 180,
+    borderRadius: 6,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 5,
+      height: 5
+    },
+    shadowOpacity: 0.25,
+    elevation: 9,
+    shadowRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  bottomCardStyle:
+  {
+    marginTop: 8,
+    width: deviceWidth-25,
+    backgroundColor: "#ED7A50",
+    height: 100,
+    borderRadius: 6,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 5,
+      height: 5
+    },
+    shadowOpacity: 0.25,
+    elevation: 9,
+    shadowRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
 
   roundButton1: {
@@ -255,5 +322,3 @@ const styles = StyleSheet.create({
 });
 
 export default Home;
-
-
