f3affe05716e4667ef9b7f2e9315fd54019ecac5
aparna124 <41099165+aparna124@users.noreply.github.com>
2021-08-09 17:03:15 -0400

    Updated UI of Payment screen

--- backend/app.js
+++ backend/app.js
@@ -12,8 +12,8 @@ app.use(cors());
 app.options('*', cors());
 
 
-const PUBLISHABLE_KEY = "ADD_PUBLISHABLE KEY HERE";
-const SECRET_KEY = "ADD_SECRETE KEY HERE";
+const PUBLISHABLE_KEY = "pk_test_51JMZBKJ05k18rayZrbN3rAiCkFTV0uzkZCqjgVUNQKNDSda44Agckge6Jwnsp0qrqan4yVdC4cCUseLUghziy90x00WZloT6aI";
+const SECRET_KEY = "sk_test_51JMZBKJ05k18rayZOkiM8D6racHeaRythKNrEcIhMDIKJgNMSbX2rHN98inBG7axnNGTo0PcWcGOmTizRmjAKy9z00oisVzoBZ";
 
 
 
--- components/StripeApp.js
+++ components/StripeApp.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, TextInput, Button, Alert } from "react-native";
+import { View, Text, StyleSheet, TextInput, Button, Alert, Image, TouchableOpacity } from "react-native";
 import { StripeProvider } from "@stripe/stripe-react-native";
 import { HOST_URL } from '../commonConfig';
 import { CardField, useConfirmPayment } from "@stripe/stripe-react-native";
+import { color } from "react-native-reanimated";
+import { ScrollView } from "react-native-gesture-handler";
 
 const API_URL = "http://localhost:3000";
 
@@ -60,8 +62,26 @@ const StripeApp = props => {
 
 return (
     <StripeProvider publishableKey="pk_test_51JMZBKJ05k18rayZrbN3rAiCkFTV0uzkZCqjgVUNQKNDSda44Agckge6Jwnsp0qrqan4yVdC4cCUseLUghziy90x00WZloT6aI">
-        <View style={styles.container}>
+      <View style={styles.container}>
+      <ScrollView>
+      <View style={styles.header}>
+        <Text style={styles.hederTitle}>Unlock your Unlimited access</Text>
+        <Image
+            style={{ width: 200, height: 200,  alignItems: 'center', justifyContent: 'center', resizeMode: 'contain'}}
+            source={require('./payment.png')}
+          />
+        <Text style={styles.hedertext}>$10/month</Text>
+      </View>
       
+      <View style={{flexDirection: 'row', alignItems: 'center', margin: 20}}>
+        <View style={{flex: 1, height: 1, backgroundColor: 'black'}} />
+        <View>
+          <Text style={{width: 100, textAlign: 'center'}}>Pay Here!</Text>
+        </View>
+        <View style={{flex: 1, height: 1, backgroundColor: 'black'}} />
+      </View>
+
+      <View style={styles.footer}>
         <TextInput
         autoCapitalize="none"
         placeholder="E-mail"
@@ -81,10 +101,16 @@ return (
           setCardDetails(cardDetails);
         }}
       />
-
-      <Button onPress={handlePayPress} title="Pay" disabled={loading} />
+      <View>
+        <TouchableOpacity style={styles.payButton} onPress={handlePayPress} disabled={loading}>
+          <Text style={styles.payBtnText}>Pay $10.00</Text>
+        </TouchableOpacity>
+      </View>
+      {/* <Button onPress={handlePayPress} title="Pay" disabled={loading} /> */}
    
         </View>
+       </ScrollView>
+      </View>
     </StripeProvider>
   );
 
@@ -96,7 +122,31 @@ export default StripeApp;
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      justifyContent: "center",
+      margin: 0,
+      backgroundColor: "#fff",
+    },
+
+    header: 
+    {
+      alignItems: 'center',
+      marginTop: 40,
+    },
+    hederTitle:
+    {
+      fontSize: 18,
+      fontWeight: '700'
+    },
+    hedertext:
+    {
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontSize: 20,
+      fontWeight: '600',
+      color: '#EB6C3E'
+
+    },
+    footer:
+    {
       margin: 20,
     },
     input: {
@@ -105,15 +155,41 @@ const styles = StyleSheet.create({
       fontSize: 20,
       height: 50,
       padding: 10,
+      marginTop: 30,
     },
 
     card: {
       backgroundColor: "#efefefef",
     },
+
     cardContainer: {
-      height: 50,
-      marginVertical: 30,
+      height: 100,
+      marginVertical: 10,
+      shadowColor: '#000',
+      shadowOffset: {
+      width: 5,
+      height: 5
+      },
+      shadowOpacity: 0.25,
+      elevation: 9,
+      shadowRadius: 5,
     },
+
+    payButton:
+    {
+      marginTop: 10,
+      width: '100%',
+      height: 50,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 5,
+      backgroundColor: '#EB6C3E'
+    },
+    payBtnText:
+    {
+      color: '#fff',
+      fontSize: 15,
+    }
   });
 
 
